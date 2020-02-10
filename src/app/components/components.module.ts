a3db9689536e9e5cd23c75daafc2a1f378b4171c
ashoktut <ashoktut.2@gmail.com>
2020-02-10 03:46:22 +0200

    first commit

---
+++ src/app/components/components.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { SlidesComponent } from './slides/slides.component';
+import { LogoComponent } from './logo/logo.component';
+import { StartComponent } from './start/start.component';
+import { IonicModule } from '@ionic/angular';
+
+
+
+@NgModule({
+  declarations: [SlidesComponent, LogoComponent, StartComponent],
+  exports: [SlidesComponent, LogoComponent, StartComponent],
+  imports: [
+    CommonModule, IonicModule
+  ]
+})
+export class ComponentsModule { }
