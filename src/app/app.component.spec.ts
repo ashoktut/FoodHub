aeba298a861bb1740ac861a595bae4fc684eb719
ashoktut <ashoktut.2@gmail.com>
2020-02-20 00:08:38 +0200

    foundation+1

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -9,7 +9,7 @@ import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
 
-  let statusBarSpy, splashScreenSpy, platformReadySpy, platformSpy;
+  let statusBarSpy: { styleDefault: any; }, splashScreenSpy: { hide: any; }, platformReadySpy: Promise<void>, platformSpy: { ready: any; };
 
   beforeEach(async(() => {
     statusBarSpy = jasmine.createSpyObj('StatusBar', ['styleDefault']);
@@ -43,5 +43,16 @@ describe('AppComponent', () => {
   });
 
   // TODO: add more tests!
+  it(`should have as title 'app'`, async(() => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.debugElement.componentInstance;
+    expect(app.title).toEqual('app');
+  }));
+  it('should render title in a h1 tag', async(() => {
+    const fixture = TestBed.createComponent(AppComponent);
+    fixture.detectChanges();
+    const compiled = fixture.debugElement.nativeElement;
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to app!');
+  }));
 
 });
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,7 +12,9 @@ import { AppComponent } from './app.component';
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule,  BrowserAnimationsModule,
+    MatToolbarModule,
+    RoutingModule],
   providers: [
     StatusBar,
     SplashScreen,
